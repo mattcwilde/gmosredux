038f0d22835417ec3a46ce9ad4694a107225cd04
Matt Wilde <mwilde@uw.edu>
2018-03-08 13:14:31 -0800

    Fixed the bias script to actually work!

--- make_bias.py
+++ make_bias.py
@@ -1,14 +1,15 @@
 """
 This script selects out the bias if its mixed into other files and generates a MasterCal Bias image
 """
-from __future__ import print, division
+from __future__ import print_function, division
 import os
 from pyraf import iraf
-from pyraf.iraf import gemini, gmos
-from gmosutils import gmosutils as gu
+from pyraf.iraf import gemini, gmos, gemtools
+import glob
+from gmosutils import gmosutils
 
 
-rawPath = 'data/raw'
+rawPath = './raw/'
 dataPath = './'
 
 if not os.path.exists("{0}/MCbiasFull.fits".format(dataPath)):
@@ -20,17 +21,20 @@ if not os.path.exists("{0}/MCbiasFull.fits".format(dataPath)):
     biasFlags = {'logfile': 'biasLog.txt', 'rawpath': rawPath,
                  'fl_vardq': 'yes', 'verbose': 'no'}
 
-
+    # find the raw fits files
     raw_files = glob.glob(rawPath+'/*fits')
-    summary = gu.observation_summary(raw_files)
-    summary[summary['OBJECT'] != 'Bias']
 
-    biasFileNameist = summary[(summary['OBSTYPE'] == 'BIAS')]['FILENAME']
-    biasFull = [os.path.basename(f) for f in bias]
+    # make an astropy table summarising them
+    summary = gmosutils.observation_summary(raw_files)
+
+    # identify only the bias files and get their filename
+    biasFileNames = summary[(summary['OBSTYPE'] == 'BIAS')]['FILENAME']
+    # convert the filename to a list
+    biasFull = [os.path.basename(f) for f in biasFileNames]
 
     if len(biasFull) > 20:
-        # write a list file
-        # IRAF cant take more that ~10 files as input
+        # write the filneame list to a file file
+        # IRAF cant take more that ~10 comma seperated files as input
         with open('biases.lis', 'w') as f:
             [f.write(x + '\n') for x in biasFull]
         gmos.gbias('@biases.lis', 'MCbiasFull.fits', **biasFlags)
@@ -40,5 +44,6 @@ if not os.path.exists("{0}/MCbiasFull.fits".format(dataPath)):
     else:
         print('Not enough BIAS images. Make sure you are in the correct folder')
 
+    print(" -- DONE: made MCbiasFull.fits")
 else:
-    print('')
+    print('Bias already exists, dummy.')
