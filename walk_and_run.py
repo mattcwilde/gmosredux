71ac4b1f37d402ffdea4cc37702f4dc8c9c6fc6b
Matt Wilde <mwilde@uw.edu>
2018-04-11 17:07:35 -0700

    os.chdir and then running the command from module doesnt seem to work...?

--- walk_and_run.py
+++ walk_and_run.py
@@ -1,7 +1,5 @@
 from __future__ import print_function, division
 import os
-# import glob
-
 # import reduce_science
 
 
@@ -12,6 +10,7 @@ def delete_file(name):
     try:
         os.remove(name)
     except OSError:
+        print('couldnt delete file:', name)
         pass
 
 path = "/Users/mwilde/Dropbox/COS-Gemini/gemini_data.GN-2014A-Q-1/reduced_data/"
@@ -21,8 +20,7 @@ for root, dirs, files in os.walk(path, topdown=True):
     for name in dirs:
         if 'name' not in ['database']:
             if 'mask' in name:
-
-                print("walking through:",os.path.join(root, name))
+                # print("walking through:",os.path.join(root, name))
 
 
                 # src = pwd+REDUX_FILE
@@ -36,7 +34,18 @@ for root, dirs, files in os.walk(path, topdown=True):
 
                 # change to this directory
                 os.chdir(os.path.join(root, name))
-                print("changed directory to:",os.path.abspath(os.path.curdir))
+
+                # print(os.getcwd())
+                # print("changed directory to:",os.path.abspath(os.path.curdir))
+
+                # run the science reduction in each mask folder
+                # reduce_science.make_2Dspec()
+                exec (open("./reduce_science.py").read())
+                print(" ======================================================== ")
+                print(" ======================================================== ")
+                print(" Finished ", os.getcwd())
+                print(" ======================================================== ")
+                print(" ======================================================== ")
 
 os.chdir(pwd)
 print("changed dir back to where we started:",os.path.abspath(os.path.curdir))
